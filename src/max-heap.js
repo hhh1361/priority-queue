05cd3817b574611fc64629b5b07dc59299c71e4e
hhh1361 <nikita.turlyko@gmail.com>
2019-03-24 23:32:34 +0300

    feat: smth to add

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,11 +2,14 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+	    this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+	    const node = new Node (data, priority);
+	    this.insertNode(node);
+	    this.shiftNodeUp(node);	
 	}
 
 	pop() {
